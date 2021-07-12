18ac8b5e1f1220857b3454f72cadee6a5d871298
Elijah Lee <elijah.lee@student.unsw.edu.au>
2021-07-12 22:57:32 +1000

    fixed style for app.py

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/get', methods=['GET'])
 def get_root():
-    return dumps({"text1":"Hello", "text2":"World"})
+    return dumps({"text1": "Hello", "text2": "World"})
 
 if __name__ == "__main__":
     app.run(host='192.168.0.10', port=3000, debug=True)
