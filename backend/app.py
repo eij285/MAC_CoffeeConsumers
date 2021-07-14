f1ea3a28565a5d9ecb8a1c61b7f86fb8f6f891ce
Pattie Chung <pat@Patties-MacBook-Pro.local>
2021-07-14 15:53:05 +1000

    test commit

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,64 @@
 from flask import Flask, request
 from json import dumps
+from user import add_user
+
 
 app = Flask(__name__)
 
-@app.route('/get', methods=['GET'])
-def get_root():
-    return dumps({"text1": "Hello", "text2": "World"})
+# Getting info from login
+@app.route('/new_usrr', methods=['GET'])
+def get_new_user():
+    # since users only need to log in once, login & register at same time
+    # request.args.get(key, default=None, type=None)
+    name = request.args.get('name')
+    home = request.args.get('home')
 
-# Make sure to replace host=... with your internal ip address
+    # ADD USER TO DICT
+    u1 = add_user(name, home)
+    
+    return dumps(u1) 
+
+# getting route update info
+@app.route('/route', methods=['GET'])
+def get_route_update():
+
+    # get new route info for each user
+
+    return dumps({"route": "update"})
+
+
+@app.route('/govt_updates', methods=['GET'])
+def get_gov_update():
+
+    # get data from govt database
+
+    return dumps({"govt" : "data"})
+
+
+# Make sure to replace host=... with your internal ip address (done)
 if __name__ == "__main__":
-    app.run(host='192.168.0.10', port=3000, debug=True)
+    app.run(debug=True)
+ 
+"""
+Possible .json file format
+    {
+        {
+            "name": "user's name"
+            "day_ls": {list of routes separated by day}
+            "home": (0.0, 0.0)
+            "is_safe": true/false
+        },
+        {
+            "name": "user's name"
+            "day_ls": {list of routes separated by day}
+            "home": (0.0, 0.0)
+            "is_safe": true/false
+        },
+        {
+            "name": "user's name"
+            "day_ls": {list of routes separated by day}
+            "home": (0.0, 0.0)
+            "is_safe": true/false
+        }
+    }
+"""
